dda89fca920dfa025c29cac0c2d2869afa999241
Chris Bruen <bruen.chris@gmail.com>
2015-12-27 11:15:32 -0500

    symbol to string

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,16 +50,16 @@ def monopoly_with_fourth_tier
 			:pieces=> 4,
 			:names=> {
 				:reading_railroad=>{
-					:mortgage_value=>"$100"
+					"mortgage_value"=>"$100"
 					},
 				:pennsylvania_railroad=>{
-					:mortgage_value=>"$200"
+					"mortgage_value"=>"$200"
 					},
 				:b_and_o_railroad=>{
-					:mortgage_value=>"$400"
+					"mortgage_value"=>"$400"
 					},
 				:shortline=>{
-					:mortgage_value=>"$800"
+					"mortgage_value"=>"$800"
 					}
 				},
 			:rent_in_dollars=>{
