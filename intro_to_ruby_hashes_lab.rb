d569aff59e3714cb4c167b9957564718766980de
Richard-Frost <frost.rchrd@gmail.com>
2015-12-28 07:47:46 -0800

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,10 +16,18 @@ monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {} }}
 end
 
 def monopoly_with_third_tier
-  monopoly = {:railroads => {:pieces => 4, :names => {:reading_railroad => {},
-              :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline => {}},
-              :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50,
-              :three_pieces_owned => 100, :four_pieces_owned => 200} }}
+  monopoly = {:railroads => {
+    :pieces => 4, 
+    :names => {
+      :reading_railroad => {},
+      :pennsylvania_railroad => {}, 
+      :b_and_o_railroad => {}, 
+      :shortline => {}},
+    :rent_in_dollars => {
+      :one_piece_owned => 25, 
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100, 
+      :four_pieces_owned => 200} }}
 end
 
 def monopoly_with_fourth_tier
