05d1e9459fb2dace36242e32eae4e739022894ce
hangloos <ericloos00@gmail.com>
2016-03-19 12:35:40 -0400

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,12 +42,20 @@ def monopoly_with_fourth_tier
     {
       :pieces => 4, 
       
-      :names => {:reading_railroad => {"mortgage_value" => "$100"}, :pennsylvania_railroad => {"mortgage_value" => "$200"}, :b_and_o_railroad => {"mortgage_value" => "$400"}, :shortline => {"mortgage_value" => "$800"}}, 
+      :names => 
+      {
+        :reading_railroad => {"mortgage_value" => "$100"}, 
+        :pennsylvania_railroad => {"mortgage_value" => "$200"}, 
+        :b_and_o_railroad => {"mortgage_value" => "$400"}, 
+        :shortline => {"mortgage_value" => "$800"}
+      }, 
       
-      :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
-
-
-
+        :rent_in_dollars => 
+      {
+        :one_piece_owned => 25, 
+        :two_pieces_owned => 50, 
+        :three_pieces_owned => 100, 
+        :four_pieces_owned => 200}
     }}
 end
 
