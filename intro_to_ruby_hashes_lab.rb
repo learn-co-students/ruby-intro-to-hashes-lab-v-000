ba5a3d7fb001740cb3968bc20268dbfd18a9f1d1
Bob Habig <rfhab2@gmail.com>
2016-05-16 22:54:33 -0400

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,14 +33,30 @@ def monopoly_with_third_tier
 	monopoly[:railroads][:rent_in_dollars][:two_pieces_owned]=50
   monopoly[:railroads][:rent_in_dollars][:three_pieces_owned]=100
 	monopoly[:railroads][:rent_in_dollars][:four_pieces_owned]=200
-	monopoly[:railroads][:name][:reading_railroad]={}
-	monopoly[:railroads][:name][:pennsylvania_railroad]={}
-	monopoly[:railroads][:name][:b_and_o_railroad]={}
-	monopoly[:railroads][:name][:shortline]={}
+	monopoly[:railroads][:names][:reading_railroad]={}
+	monopoly[:railroads][:names][:pennsylvania_railroad]={}
+	monopoly[:railroads][:names][:b_and_o_railroad]={}
+	monopoly[:railroads][:names][:shortline]={}
 	monopoly
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-
+	monopoly = {}
+	monopoly[:railroads]={}
+	monopoly[:railroads][:pieces]=4
+	monopoly[:railroads][:names]={}
+	monopoly[:railroads][:rent_in_dollars]={}
+	monopoly[:railroads][:rent_in_dollars][:one_piece_owned]=25
+	monopoly[:railroads][:rent_in_dollars][:two_pieces_owned]=50
+	monopoly[:railroads][:rent_in_dollars][:three_pieces_owned]=100
+	monopoly[:railroads][:rent_in_dollars][:four_pieces_owned]=200
+	monopoly[:railroads][:names][:reading_railroad]={}
+	monopoly[:railroads][:names][:pennsylvania_railroad]={}
+	monopoly[:railroads][:names][:b_and_o_railroad]={}
+	monopoly[:railroads][:names][:shortline]={}
+	monopoly[:railroads][:names][:reading_railroad]["mortgage_value"]= "$100"
+	monopoly[:railroads][:names][:pennsylvania_railroad]["mortgage_value"]= "$200"
+	monopoly[:railroads][:names][:b_and_o_railroad]["mortgage_value"]= "$400"
+	monopoly[:railroads][:names][:shortline]["mortgage_value"]= "$800"
+	monopoly
 end
