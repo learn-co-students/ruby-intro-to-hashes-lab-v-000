f5cc7a71a1657ac5761658aac009b8694b20eb88
Senad Hadrovic <senadhadrovic@gmail.com>
2016-11-21 18:04:55 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,31 +1,30 @@
-def new_hash
+require 'pry'
 
+def new_hash
+ new_hash = {}
 end
 
 def actor
-
+actor = {:name => 'Dwayne The Rock Johnson'}
 end
 
 def monopoly
 	monopoly = {}
-
+	binding.pry
+  monopoly[:railroads] = {}
 end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	
+
 end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		
+
 end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-	
+
 end
-
-
-
-
