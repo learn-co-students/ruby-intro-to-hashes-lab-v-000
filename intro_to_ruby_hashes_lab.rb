8edbaba4cb8143a6294b52265b2ed4a0ef594d26
An Nuyen <annuyen@gmail.com>
2017-12-30 19:44:43 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,31 +1,82 @@
 def new_hash
-
+  new_hash = {}
+  new_hash
 end
 
 def actor
-
+  actor = {:name => "Dwayne The Rock Johnson"}
+  actor
 end
 
 def monopoly
-	monopoly = {}
-
+	monopoly = {:railroads => {}}
+  monopoly
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	
+	monopoly_with_second_tier = {
+    :railroads =>
+    {
+      :pieces => 4,
+      :names => {},
+      :rent_in_dollars => {}
+    }
+  }
+  monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		
+  monopoly_with_third_tier = {
+    :railroads =>
+    {
+      :pieces => 4,
+      :names => {
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortline => {}
+      },
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      }
+    }}
+  monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-	
+  monopoly_with_fourth_tier = {
+    :railroads =>
+    {
+      :pieces => 4,
+      :names =>
+      {
+        :reading_railroad =>
+        {
+          "mortgage_value"=>"$100"
+        },
+        :pennsylvania_railroad =>
+        {
+          "mortgage_value"=>"$200"
+        },
+        :b_and_o_railroad =>
+        {
+          "mortgage_value"=>"$400"
+        },
+        :shortline =>
+        {
+          "mortgage_value"=>"$800"
+        }
+      },
+      :rent_in_dollars =>
+      {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      }
+    }}
+  monopoly_with_fourth_tier
 end
-
-
-
-
