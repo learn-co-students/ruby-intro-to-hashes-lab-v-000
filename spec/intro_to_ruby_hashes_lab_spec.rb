8f657a343ba85afb87728e76ae4ab09c33d233ea
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-06 02:29:20 +0000

    Done.

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -121,28 +121,28 @@ describe "building a multidimensional monopoly hash" do
 
   describe "fourth level of monopoly hash" do
     describe "#monopoly_with_fourth_tier" do
-      xit "sets the 1st key of the :reading_railroad hash to a string, 'mortgage_value', whose value is '$100'" do
+       it "sets the 1st key of the :reading_railroad hash to a string, 'mortgage_value', whose value is '$100'" do
 
         expect(monopoly_with_fourth_tier.values[0][:names].has_key?(:reading_railroad)).to be true
         expect(monopoly_with_fourth_tier.values[0][:names].values[0].has_key?("mortgage_value")).to be true
         expect(monopoly_with_fourth_tier.values[0][:names].values[0].values[0]).to eq("$100")
       end
 
-      xit "sets the 1st key of the :pennsylvania_railroad hash to a string, 'mortgage_value', whose value is '$200'" do
+       it "sets the 1st key of the :pennsylvania_railroad hash to a string, 'mortgage_value', whose value is '$200'" do
 
         expect(monopoly_with_fourth_tier.values[0][:names].has_key?(:pennsylvania_railroad)).to be true
         expect(monopoly_with_fourth_tier.values[0][:names].values[1].has_key?("mortgage_value")).to be true
         expect(monopoly_with_fourth_tier.values[0][:names].values[1].values[0]).to eq("$200")
       end
 
-      xit "sets the 1st key of the :b_and_o_railroad hash to a string, 'mortgage_value', whose value is '$400'" do
+      it "sets the 1st key of the :b_and_o_railroad hash to a string, 'mortgage_value', whose value is '$400'" do
 
         expect(monopoly_with_fourth_tier.values[0][:names].has_key?(:b_and_o_railroad)).to be true
         expect(monopoly_with_fourth_tier.values[0][:names].values[2].has_key?("mortgage_value")).to be true
         expect(monopoly_with_fourth_tier.values[0][:names].values[2].values[0]).to eq("$400")
       end
 
-      xit "sets the 1st key of the :shortline hash to a string, `mortgage_value`, whose value is '$800'" do
+       it "sets the 1st key of the :shortline hash to a string, `mortgage_value`, whose value is '$800'" do
 
         expect(monopoly_with_fourth_tier.values[0][:names].keys[3]).to eq(:shortline)
         expect(monopoly_with_fourth_tier.values[0][:names].values[3].has_key?("mortgage_value")).to be true
