ddacaf2dab780c6edd0f72ac1dfb98d4835defd7
Katie Cassell <kcassell944@gmail.com>
2018-02-26 19:51:01 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,11 @@
 def new_hash
+	{}
 
 end
 
 def actor
+	actor[:name]
+
 
 end
 
@@ -13,19 +16,15 @@ end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	
+
 end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		
+
 end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-	
+
 end
-
-
-
-
