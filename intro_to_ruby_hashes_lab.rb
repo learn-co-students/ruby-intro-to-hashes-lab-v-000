cd6cbf41ab231193c66d2bc0b1a3548bac6febe9
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-06 17:34:48 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,9 @@
-require 'pry'
 def new_hash
 	{}
 end
 
 def actor
-	acter = {name: 'Dwayne The Rock Johnson'}
+	{name: 'Dwayne The Rock Johnson'}
 end
 
 def monopoly
@@ -22,14 +21,15 @@ def monopoly_with_third_tier
 		rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
 	}}
 end
+
 def monopoly_with_fourth_tier
 	{railroads: {
 		pieces: 4,
 		names: {
-			reading_railroad: {mortgage_value: '$100'},
-			pennsylvania_railroad: {},
-			b_and_o_railroad: {},
-			shortline: {}
+			reading_railroad: {"mortgage_value" => "$100"},
+			pennsylvania_railroad: {"mortgage_value" => "$200"},
+			b_and_o_railroad: {"mortgage_value" => "$400"},
+			shortline: {"mortgage_value" => "$800"}
 		},
 		rent_in_dollars: {
 			one_piece_owned: 25,
@@ -39,5 +39,3 @@ def monopoly_with_fourth_tier
 		}
 	}}
 end
-
-# binding.pry
