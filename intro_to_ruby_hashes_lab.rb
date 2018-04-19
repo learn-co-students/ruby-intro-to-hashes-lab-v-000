8a35dc602eba86d39ca7fc162cf80d7df0311f48
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-19 04:32:10 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,10 +36,10 @@ end
 def monopoly_with_fourth_tier
   monopoly = {:railroads =>
     {:pieces => 4,
-    :names => {:reading_railroad => {:mortgage_value => '$100'},
-      :pennsylvania_railroad => {:mortgage_value => '$200'},
-      :b_and_o_railroad => {:mortgage_value => '$400'},
-      :shortline => {:mortgage_value => '$800'}},
+    :names => {:reading_railroad => {"mortgage_value" => "$100"},
+      :pennsylvania_railroad => {"mortgage_value" => "$200"},
+      :b_and_o_railroad => {"mortgage_value" => "$400"},
+      :shortline => {"mortgage_value" => "$800"}},
     :rent_in_dollars =>
     {:one_piece_owned => 25,
       :two_pieces_owned => 50,
