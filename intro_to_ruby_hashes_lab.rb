4b50794e8f22fa67a7e9d0e36bd3a8c3737fc150
Valencia Caldwell <vkcaldwell@gmail.com>
2018-04-22 22:02:00 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,7 @@ end
 def monopoly_with_fourth_tier
 	monopoly = {railroads:
 		{pieces: 4,
-			names: {reading_railroad:{mortgage_value: "$100"},
+			names: {reading_railroad:{"#{mortgage_value}": "$100"},
 		pennsylvania_railroad:{},
 		b_and_o_railroad:{},
 		shortline:{}
