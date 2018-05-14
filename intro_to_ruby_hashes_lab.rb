189dc231b87a786df9f927db854e7209f0a76063
Dale Terrell <tragicid@gmail.com>
2018-05-14 16:28:52 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,24 +1,39 @@
 def new_hash
-
+  {}
 end
 
 def actor
-
+  # example hash: my_hash = {:name => "Howard"}
+  # a variable, actor, that holds a hash
+  # this hash has a key of name, whose value is "Dwayne The Rock Johnson"
+  actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {
+	  railroads: {}
+	}
 
 end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	
+	monopoly = {
+	  railroads: {
+	    :pieces => 4
+	  }
+	}
 end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		
+	
+	monopoly = {
+	  railroads: {
+	    :names => ({})
+	  }
+  }
 end
 
 def monopoly_with_fourth_tier
