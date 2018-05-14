972ac80814574421d34eb0e1d50c1a9dc988e407
Dale Terrell <tragicid@gmail.com>
2018-05-14 16:50:03 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
 monopoly = {
 	  railroads: {
 	    :pieces => 4,
-	    :names => {},
+	    :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline => {}},
 	    :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
 	  }
 	}
