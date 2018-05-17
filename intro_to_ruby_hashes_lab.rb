5b75c6f9178c528f82a3a07a3b7d1004db8150a0
Nathaniel Shuster <nshu521992@gmail.com>
2018-05-17 05:11:35 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,24 +48,31 @@ end
 
 def monopoly_with_fourth_tier
   monopoly = {
-	  railroads: {
+	  railroads: 
+	  {
 	    pieces: 4,
-	    names: {
-	      reading_railroad: {
+	    names: 
+	    {
+	      reading_railroad: 
+	      {
 	        "mortgage_value" => "$100"
 	      },
-	      pennsylvania_railroad: {
+	      pennsylvania_railroad: 
+	      {
 	        "mortgage_value" => "$200"
 	      },
-	      b_and_o_railroad: {
+	      b_and_o_railroad: 
+	      {
 	        "mortgage_value" => "$400"
 	      },
-	      shortline: {
+	      shortline: 
+	      {
 	        "mortgage_value" => "$800"
 	      }
 	    },
 	    
-	    rent_in_dollars: {
+	    rent_in_dollars: 
+	    {
 	      one_piece_owned: 25,
 	      two_pieces_owned: 50,
 	      three_pieces_owned: 100,
