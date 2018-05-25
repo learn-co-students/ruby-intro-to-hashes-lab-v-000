2d9e7496cf00cfffb5c30842cf804af64e97e81b
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-25 00:54:27 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,9 +51,9 @@ def monopoly_with_fourth_tier
 			pieces: 4,
 			names: {
 				reading_railroad: {"mortgage_value" => "$100"},
-				pennsylvania_railroad: {},
-				b_and_o_railroad: {},
-				shortline: {}
+				pennsylvania_railroad: {"mortgage_value" => "$200"},
+				b_and_o_railroad: {"mortgage_value" => "$400"},
+				shortline: {"mortgage_value" => "$800"}
 			},
 			rent_in_dollars: {
 				one_piece_owned: 25,
