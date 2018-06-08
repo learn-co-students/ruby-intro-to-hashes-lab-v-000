af79a1c91edb1a9f9124431f7375d43cc6231134
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-06-08 13:30:53 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,12 +3,29 @@ def new_hash
   return my_new_hash
 end
 
-def actor 
+def actor  
   :name => {"Dwayne The Rock Johnson"}
+  return :name
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {
+	  :railroads => {
+	    :pieces => 4,
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25,
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
+	    },
+	    :names => {
+	      :reading_railroad => {
+	        "mortgage_value" = "$100"
+	      },
+	      :pennsylvania_railroad =>
+	    }
+	  }
+	}
 
 end
 
