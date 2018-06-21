e56dc78c3fad92aa8404326fb36dd11409b849b5
Michael Blandford <mdblandford@gmail.com>
2018-06-21 15:45:23 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,37 +7,34 @@ def actor
 end
 
 def monopoly
-	monopoly = {:railroads => {}}
+	monopoly = {railroads: {}}
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {:pieces =>4, :names => {}, :rent_in_dollars => {}}}
+	monopoly = {railroads: {pieces: 4, names: {}, rent_in_dollars: {}}}
 end
 
 def monopoly_with_third_tier
-	monopoly = {:railroads => {:pieces =>4, :names => {:reading_railroad=>{}, :pennsylvania_railroad=>{}, :b_and_o_railroad=>{}, :shortline=>{}}, :rent_in_dollars => {:one_piece_owned=>25, :two_pieces_owned=>50, :three_pieces_owned=>100, :four_pieces_owned=>200}}}	
+	monopoly = {railroads: {pieces: 4, names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline: {}},
+  rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}}}
 end
 
 def monopoly_with_fourth_tier
-monopoly = {:railroads => 
-  {:pieces =>4, 
-  :names =>   
+monopoly = {:railroads =>
+  {:pieces =>4,
+  :names =>
     {:reading_railroad=>
-      {"mortgage_value"=>"$100"}, 
+      {"mortgage_value"=>"$100"},
     :pennsylvania_railroad=>
-      {"mortgage_value"=>"$200"}, 
+      {"mortgage_value"=>"$200"},
     :b_and_o_railroad=>
-      {"mortgage_value"=>"$400"}, 
+      {"mortgage_value"=>"$400"},
     :shortline=>
-      {"mortgage_value"=>"$800"}}, 
-  :rent_in_dollars => 
-  {:one_piece_owned=>25, 
-  :two_pieces_owned=>50, 
-  :three_pieces_owned=>100, 
+      {"mortgage_value"=>"$800"}},
+  :rent_in_dollars =>
+  {:one_piece_owned=>25,
+  :two_pieces_owned=>50,
+  :three_pieces_owned=>100,
   :four_pieces_owned=>200}
-  }}	
+  }}
 end
-
-
-
-
