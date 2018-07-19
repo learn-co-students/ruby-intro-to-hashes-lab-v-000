11ef1e29b2cc8014cf3cd5e094addc8aa197e374
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-19 03:21:38 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,18 +13,16 @@ end
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 	monopoly_with_second_tier = { 
-	  :railroads => 
+	  :railroads =>
 	  {
-  	  :pieces => 4, 
-      :rent_in_dollars =>
-      {
-        
-      },
-      :names =>
-      {
-        
-      }
+  	  :pieces => 4,
+    	:names => {
   	  
+    	},
+    	:rent_in_dollars => {
+    	  
+    	}
+
   	} 
 	}
 
@@ -32,36 +30,35 @@ end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		monopoly_with_second_tier = { 
-	  :railroads => 
+		monopoly_with_third_tier = { 
+	  :railroads =>
 	  {
-  	  :pieces => 4, 
-      :rent_in_dollars =>
-      {
-        :one_piece_owned=>25,
-        :two_pieces_owned=>50,
-        :three_pieces_owned=>100,
-        :four_pieces_owned=>200
-      },
-      :names =>
-      {
-        :reading_railroad=>
-      {
+  	  :pieces => 4,
+    	:names => {
+  	    :reading_railroad => {
+  	      
+  	    },
+        :pennsylvania_railroad=>
+        {
 
-      },
-      :pennsylvania_railroad=>
-      {
+        },
+        :b_and_o_railroad=>
+        {
 
-      },
-      :b_and_o_railroad=>
-      {
+        },
+        :shortline=>
+        {
 
-      },
-      :shortline=>
-      {
+        }
+    	},
+    	:rent_in_dollars => {
+    	  :one_piece_owned => 25,
+    	  :two_pieces_owned => 50,
+    	  :three_pieces_owned => 100,
+        :four_pieces_owned => 200
 
-      }
-  	  
+    	}
+
   	} 
 	}
 
@@ -70,7 +67,7 @@ end
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 		monopoly_with_fourth_tier = { 
-  :railroads=>
+ :railroads=>
   {
     :pieces=>4,
     :rent_in_dollars=>
@@ -101,6 +98,7 @@ def monopoly_with_fourth_tier
     }
   }
 }
+
 end
 
 
