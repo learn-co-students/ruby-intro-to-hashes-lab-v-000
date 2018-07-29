d09a25b0bc912f65084a4b845095521de3560956
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-29 23:32:41 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,18 +45,10 @@ def monopoly_with_fourth_tier
 		railroads: {
 			pieces: 4,
 			names: {
-				reading_railroad: {
-					"mortgage_value" => "$100"
-				},
-				pennsylvania_railroad: {
-					"mortgage_value" => "$200"
-				},
-				b_and_o_railroad: {
-					"mortgage_value" => "$400"
-				},
-				shortline: {
-					"mortgage_value" => "$800"
-				}
+				reading_railroad: {"mortgage_value" => "$100"},
+				pennsylvania_railroad: {"mortgage_value" => "$200"},
+				b_and_o_railroad: {"mortgage_value" => "$400"},
+				shortline: {"mortgage_value" => "$800"}
 			},
 			rent_in_dollars: {
 				one_piece_owned: 25,
