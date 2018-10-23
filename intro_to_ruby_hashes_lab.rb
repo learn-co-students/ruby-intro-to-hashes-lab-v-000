63826720ec7e826127361956259efa662d6b0018
Paula McKinley <pmknyc@optimum.net>
2018-10-23 18:19:38 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,26 +21,6 @@ def monopoly_with_third_tier
 		{:railroads => {
 				:pieces => 4,
 				:names => {
-					:reading_railroad => {'mortgage_value' => '$100'},
-					:pennsylvania_railroad => {'mortgage_value' => '$200'},
-					:b_and_o_railroad => {'mortgage_value' => '$400'},
-					:shortline => {'mortgage_value' => '$800'}
-					},
-				:rent_in_dollars => {
-					:one_piece_owned=> 25,
-					:two_pieces_owned => 50,
-					:three_pieces_owned => 100,
-					:four_pieces_owned => 200}
-					}
-				}
-end
-
-def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
-	monopoly =
-		{:railroads => {
-				:pieces => 4,
-				:names => {
 					:reading_railroad => {},
 					:pennsylvania_railroad => {},
 					:b_and_o_railroad => {},
@@ -52,5 +32,24 @@ def monopoly_with_fourth_tier
 					:three_pieces_owned => 100,
 					:four_pieces_owned => 200}
 					}
+			}
+end
+
+def monopoly_with_fourth_tier
+	monopoly =
+		{:railroads => {
+				:pieces => 4,
+				:names => {
+					:reading_railroad => {'mortgage_value' => '$100'},
+					:pennsylvania_railroad => {'mortgage_value' => '$200'},
+					:b_and_o_railroad => {'mortgage_value' => '$400'},
+					:shortline => {'mortgage_value' => '$800'}
+				},
+				:rent_in_dollars => {
+					:one_piece_owned=> 25,
+					:two_pieces_owned => 50,
+					:three_pieces_owned => 100,
+					:four_pieces_owned => 200}
 				}
+			}
 end
